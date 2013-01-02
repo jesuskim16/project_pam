066913a19b1090430ce03924aa07e17428e9e22d
JongGyu <Innovation.CIO.Leader@gamil.com>
2013-01-02 17:07:53 +0900

    js 추가

---
+++ WebContent/js/sales_insert.js
@@ -0,0 +1,7 @@
+var openZipcode = function() {
+	var url = "Zipcode.do";
+	open(url, "confirm", 
+		"toolbar=no, location=no, status=no, menubar=no, scrolbar=yes" +
+		"resizeble=no, width=450, height=400");
+	
+}
