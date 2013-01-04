ff231aa970d1fdf6f0b0bcdba164f01aa307c6b0
taeone <jtae1@naver.com>
2013-01-04 16:39:36 +0900

    DB수익부분 추가

---
+++ WebContent/rebate.sql
@@ -0,0 +1,26 @@
+INSERT INTO rebate(seq, model_code, lte34, lte42, lte52, lte62, lte72, lte100)
+VALUES(rebate_seq.nextval,'iPhone5 64G','30000','30000','50000','80000','80000','120000');
+INSERT INTO rebate(seq, model_code, lte34, lte42, lte52, lte62, lte72, lte100)
+VALUES(rebate_seq.nextval,'iPhone5 32G','30000','30000','50000','80000','80000','120000');
+INSERT INTO rebate(seq, model_code, lte34, lte42, lte52, lte62, lte72, lte100)
+VALUES(rebate_seq.nextval,'iPhone5 16G','30000','30000','50000','80000','80000','120000');
+INSERT INTO rebate(seq, model_code, lte34, lte42, lte52, lte62, lte72, lte100)
+VALUES(rebate_seq.nextval,'iPhone5 64GW','30000','30000','50000','80000','80000','120000');
+INSERT INTO rebate(seq, model_code, lte34, lte42, lte52, lte62, lte72, lte100)
+VALUES(rebate_seq.nextval,'iPhone5 32GW','30000','30000','50000','80000','80000','120000');
+INSERT INTO rebate(seq, model_code, lte34, lte42, lte52, lte62, lte72, lte100)
+VALUES(rebate_seq.nextval,'iPhone5 16GW','30000','30000','50000','80000','80000','120000');
+INSERT INTO rebate(seq, model_code, lte34, lte42, lte52, lte62, lte72, lte100)
+VALUES(rebate_seq.nextval,'LG-F180S','80000','80000','120000','160000','160000','200000');
+INSERT INTO rebate(seq, model_code, lte34, lte42, lte52, lte62, lte72, lte100)
+VALUES(rebate_seq.nextval,'LG-F200S','80000','80000','120000','160000','160000','200000');
+INSERT INTO rebate(seq, model_code, lte34, lte42, lte52, lte62, lte72, lte100)
+VALUES(rebate_seq.nextval,'SHV-E250S-64G','50000','50000','80000','120000','120000','180000');
+INSERT INTO rebate(seq, model_code, lte34, lte42, lte52, lte62, lte72, lte100)
+VALUES(rebate_seq.nextval,'SHV-E250S-64GW','50000','50000','80000','120000','120000','180000');
+INSERT INTO rebate(seq, model_code, lte34, lte42, lte52, lte62, lte72, lte100)
+VALUES(rebate_seq.nextval,'SHV-E250S-32G','50000','50000','80000','120000','120000','180000');
+INSERT INTO rebate(seq, model_code, lte34, lte42, lte52, lte62, lte72, lte100)
+VALUES(rebate_seq.nextval,'SHV-E250S-32GW','50000','50000','80000','120000','120000','180000');
+INSERT INTO rebate(seq, model_code, lte34, lte42, lte52, lte62, lte72, lte100)
+VALUES(rebate_seq.nextval,'IM-A850S','100000','100000','150000','200000','200000','250000');
