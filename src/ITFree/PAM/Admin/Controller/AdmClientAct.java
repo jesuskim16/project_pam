de219c011a5690e717dabb01db29e7fe12eea57f
A <A@organiza-5d9d9d>
2013-01-15 09:59:07 +0900

    관리자(고객)

--- src/ITFree/PAM/Admin/Controller/AdmClientAct.java
+++ src/ITFree/PAM/Admin/Controller/AdmClientAct.java
@@ -2,6 +2,7 @@ package ITFree.PAM.Admin.Controller;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,18 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import ITFree.PAM.Admin.Model.AdmClient.AdmClientDao;
+import ITFree.PAM.Admin.Model.AdmClient.AdmClientDaoImp;
 import ITFree.PAM.Admin.Model.AdmClient.AdmClientDto;
 
 @Controller
-public class AdminClientAct {
+public class AdmClientAct {
 	
-	@Autowired
-	private AdmClientDao Cdao;
+	private Logger log = Logger.getLogger(getClass());
+
 	
-	@RequestMapping("admClientList.do")
-	public ModelAndView admClientList(@ModelAttribute AdmClientDto Cdto, ModelAndView mav){
+	@RequestMapping("/admClientList.do")
+	public ModelAndView admClientList(@ModelAttribute AdmClientDto ACdto, ModelAndView mav){
+		AdmClientDao ACdao = new AdmClientDaoImp();
 		
-		List<AdmClientDto> list = Cdao.AdmClientList();
+		log.debug("--"+ACdto);
+		
+		List<AdmClientDto> list = ACdao.AdmClientList();
+		
+		log.debug("--"+list);
 		
 		mav.setViewName("/WEB-INF/www/admin/client/list.jsp");
 		
