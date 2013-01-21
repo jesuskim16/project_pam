6d2f3abd8bf9edaa45e57f443fdbd84c53be886b
taeone <jtae1@naver.com>
2013-01-21 10:08:34 +0900

    log4j 경로

--- src/ITFree/PAM/Common/Controller/Board/PriceInfoAct.java
+++ src/ITFree/PAM/Common/Controller/Board/PriceInfoAct.java
@@ -1,27 +1,17 @@
 package ITFree.PAM.Common.Controller.Board;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.List;
 
-import javax.annotation.processing.FilerException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
 import ITFree.PAM.Common.Model.Board.BoardDao;
