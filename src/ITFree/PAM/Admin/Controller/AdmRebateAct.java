17865dbad7ebc86a59435a905020233f05bdebc7
taeone <jtae1@naver.com>
2013-01-29 16:29:51 +0900

    관리자(수익표)...

--- src/ITFree/PAM/Admin/Controller/AdmRebateAct.java
+++ src/ITFree/PAM/Admin/Controller/AdmRebateAct.java
@@ -3,6 +3,7 @@ package ITFree.PAM.Admin.Controller;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,13 +26,36 @@ public class AdmRebateAct {
 	public String admRebateList(Model model){
 		log.debug("---start["+"AdmRebateAct."+"admRebateList"+"]");
 		
-		ArrayList<String> priceName = arbDao.getPriceName();	//요금제 이름을 가져온다
-		log.debug("--priceName:"+priceName);
-		List<HashMap<String,String>> list = (ArrayList<HashMap<String,String>>) arbDao.getPriceTable((List<String>)priceName);
-		log.debug("--list:"+list);
+		ArrayList<String> priceName = priceName();
+		
 		model.addAttribute("priceName",priceName);
-		model.addAttribute("list",list);
+		model.addAttribute("list",list(priceName));
 		return "/WEB-INF/www/admin/rebate/list.jsp";
 	}	
+	@RequestMapping("admRebateUpdate.do")
+	public String admRebateUpdate(Model model, String seq, String price, String money){
+		log.debug("---start["+"AdmRebateAct."+"admRebateUpdate"+"]");
+		log.debug("--"+seq+":"+price+":"+money);		
+		int iSeq = Integer.parseInt(seq);
+		
+		log.debug("--"+iSeq+":"+price+":"+money);
+		Map<String,Object> updateInfo = new HashMap<String,Object>();
+		updateInfo.put("seq", iSeq);
+		updateInfo.put("price", price);
+		updateInfo.put("money", money);
+		boolean updateResult = arbDao.updatePrice(updateInfo);
+		
+		model.addAttribute("priceName",priceName());
+		model.addAttribute("list",list(priceName()));
+		return "/WEB-INF/www/admin/rebate/list.jsp";
+	}
+
+	public ArrayList<String> priceName(){	//요금제 리스트
+		return arbDao.getPriceName();
+	}	
+	
+	public List<HashMap<String,String>>list(ArrayList<String> priceName){	//리베이트 리스트
+		return (ArrayList<HashMap<String,String>>)arbDao.getPriceTable((List<String>)priceName);		
+	}
 	
 }
